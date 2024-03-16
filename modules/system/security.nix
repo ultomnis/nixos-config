a6c279ecd2b05fb391e5857d083b8532da122168
ultomnis <125839032+ultomnis@users.noreply.github.com>
2024-03-16 08:43:36 -0400

    Remove tpm2

--- modules/system/security.nix
+++ modules/system/security.nix
@@ -1,12 +1,4 @@
 {
   # Swaylock PAM authentication
   security.pam.services.swaylock = {};
-
-  # Trusted Platform Module
-  security.tpm2 = {
-    enable = true;
-    pkcs11.enable = true;
-    tctiEnvironment.enable = true;
-  };
-  users.users.user.extraGroups = [ "tss" ];
 }
