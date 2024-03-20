3a5839ce25d508f32438c2015798e8991e097c30
ultomnis <125839032+ultomnis@users.noreply.github.com>
2024-03-20 11:52:19 -0400

    Enable nm-applet

--- modules/system/networking.nix
+++ modules/system/networking.nix
@@ -3,6 +3,9 @@
   networking.networkmanager.enable = true;
   users.users.user.extraGroups = [ "networkmanager" ];
 
+  # network-manager-applet
+  programs.nm-applet.enable = true;
+
   # OpenSSH
   services.openssh.enable = true;
 }
