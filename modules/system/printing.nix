992f9d59054e7ba9f2038ea227f3d4e3cd2dd39a
ultomnis <125839032+ultomnis@users.noreply.github.com>
2024-03-20 17:51:20 -0400

    Enable cups-pdf backend

--- modules/system/printing.nix
+++ modules/system/printing.nix
@@ -1,6 +1,12 @@
 {
-  # Enable local print service
-  services.printing.enable = true;
+  # Local print service
+  services.printing = {
+    enable = true;
+    
+    cups-pdf = {
+      enable = true;
+    };
+  };
 
   # Autodiscovery of network printers
   services.avahi = {
