5c5abf9925392d38d2642aee4347d4c2d8ba0993
ultomnis <125839032+ultomnis@users.noreply.github.com>
2024-03-23 15:55:09 -0400

    Add xdg-desktop-portal-gtk

--- modules/hyprland.nix
+++ modules/hyprland.nix
@@ -6,4 +6,10 @@
     enable = true;
     package = inputs.hyprland.packages.${pkgs.system}.hyprland;
   };
+
+  # XDG Desktop Portal  
+  xdg.portal = {
+    enable = true;
+    extraPortals = [ pkgs.xdg-desktop-portal-gtk ];
+  };
 }
