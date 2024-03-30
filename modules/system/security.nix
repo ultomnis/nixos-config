2e17f746d89d14cfa0959271151f879e6ba14cd0
ultomnis <125839032+ultomnis@users.noreply.github.com>
2024-03-30 16:01:38 -0400

    Unlock keyring upon login

--- modules/system/security.nix
+++ modules/system/security.nix
@@ -1,7 +1,12 @@
 {
   security = {
-    # Hyprlock PAM authentication
-    pam.services.hyprlock = { };
+    pam.services = {
+      # Unlock GNOME Keyring upon login
+      greetd.enableGnomeKeyring = true;
+    
+      # Hyprlock PAM authentication
+      hyprlock = { };
+    };
 
     # Realtime policy and watchdog daemon
     rtkit.enable = true;
