007f508dcb5bfb773e27def0a7d7f0066dbf5aa2
ultomnis <125839032+ultomnis@users.noreply.github.com>
2024-03-30 15:34:36 -0400

    Remove GTK settings and refactor dconf

---
+++ home/environment/dconf.nix
@@ -0,0 +1,10 @@
+{
+  # dconf
+  dconf.settings = {
+    # Dark theme
+    "org/gnome/desktop/interface" = {
+      color-scheme = "prefer-dark";
+    };
+  };
+}
+
--- home/environment/default.nix
+++ home/environment/default.nix
@@ -1,10 +1,10 @@
 {
   imports = [
+    ./dconf.nix
     ./dunst.nix
     ./fish.nix
     ./fuzzel.nix
     ./gammastep.nix
-    ./gnome.nix
     ./hypridle.nix
     ./hyprland.nix
     ./hyprlock.nix
