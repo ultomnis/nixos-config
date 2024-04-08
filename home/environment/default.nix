c45884187ad3c3fc584b7f526056369f65abe5d6
ultomnis <125839032+ultomnis@users.noreply.github.com>
2024-04-07 20:04:12 -0400

    Replace dunst with mako

--- home/environment/default.nix
+++ home/environment/default.nix
@@ -1,13 +1,13 @@
 {
   imports = [
     ./dconf.nix
-    ./dunst.nix
     ./fish.nix
     ./fuzzel.nix
     ./gammastep.nix
     ./hypridle.nix
     ./hyprland.nix
     ./hyprlock.nix
+    ./mako.nix
     ./waybar.nix
     ./wlogout.nix
   ];
---
+++ home/environment/mako.nix
@@ -0,0 +1,10 @@
+{
+  # Notification daemon
+  services.mako = {
+    enable = true;
+    backgroundColor = "#000000bf";
+    borderColor = "#ffffffff";
+    defaultTimeout = 5000;
+  };
+}
+
