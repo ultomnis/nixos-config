11c901e0779036c17641de54116eb4730a073e12
ultomnis <125839032+ultomnis@users.noreply.github.com>
2024-06-08 08:36:37 -0400

    Adapt rpfilter

--- modules/system/networking.nix
+++ modules/system/networking.nix
@@ -1,13 +1,8 @@
 { userConfig, ... }:
 
 {
-  networking = {
-    # Enable NetworkManager
-    networkmanager.enable = true;
-
-    # Reverse path filtering
-    firewall.checkReversePath = "loose";
-  };
+  # Enable NetworkManager
+  networking.networkmanager.enable = true;
   users.users.${userConfig.username}.extraGroups = [ "networkmanager" ];
 
   programs.nm-applet.enable = true;
