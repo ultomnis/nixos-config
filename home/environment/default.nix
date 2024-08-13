5e303efac64bcaf98bc2fd225e756b5bcd9d2966
ultomnis <125839032+ultomnis@users.noreply.github.com>
2024-08-13 15:29:13 -0400

    Enable gammastep

--- home/environment/default.nix
+++ home/environment/default.nix
@@ -2,6 +2,7 @@
   imports = [
     ./fish.nix
     ./fuzzel.nix
+    ./gammastep.nix
     ./gnome.nix
     ./hypridle.nix
     ./hyprland.nix
---
+++ home/environment/gammastep.nix
@@ -0,0 +1,14 @@
+{
+  # Adjusts color temperature of the screen
+  services.gammastep = {
+    enable = true;
+    latitude = 40.7;
+    longitude = -74.0;
+
+    settings = {
+      general = {
+        adjustment-method = "wayland";
+      };
+    };
+  };
+}
