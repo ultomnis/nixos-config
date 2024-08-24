e7f380cfd1c50b14eb3c87c1c2746c8cd523ce62
ultomnis <125839032+ultomnis@users.noreply.github.com>
2024-08-24 13:19:20 -0400

    homebrew: Configure updates

--- modules/darwin/programs/homebrew.nix
+++ modules/darwin/programs/homebrew.nix
@@ -2,9 +2,17 @@
   # Package manager
   homebrew = {
     enable = true;
-    onActivation.autoUpdate = true;
-    onActivation.cleanup = "zap";
-    onActivation.upgrade = true;
+
+    # Behavior of brew bundle command
+    onActivation = {
+      cleanup = "zap";
+      upgrade = true;
+    };
+
+    # Behavior of manually invoked Homebrew commands
+    global = {
+      autoUpdate = false;
+    };
 
     casks = [
       "aerospace"
