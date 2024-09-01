c07fdd315c5f2acfae445ac32112dfd93bf23632
ultomnis <125839032+ultomnis@users.noreply.github.com>
2024-09-01 10:04:34 -0400

    mako: Increase opacity

--- home/nixos/environment/mako.nix
+++ home/nixos/environment/mako.nix
@@ -2,7 +2,7 @@
   # Notification daemon
   services.mako = {
     enable = true;
-    backgroundColor = "#00000080";
+    backgroundColor = "#000000cc";
     borderColor = "#f0f0f0ff";
     defaultTimeout = 5000;
   };
