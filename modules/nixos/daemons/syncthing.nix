2d45eeafd0a348755935aa9984ac588fe55fbc82
ultomnis <125839032+ultomnis@users.noreply.github.com>
2024-10-02 15:20:54 -0400

    syncthing: Add device

--- modules/nixos/daemons/syncthing.nix
+++ modules/nixos/daemons/syncthing.nix
@@ -15,13 +15,14 @@
     settings = {
       devices = {
         "desktop" = { id = "SOYT7RW-KCZK6LA-TMADIQX-C3NDTWW-LO5ABQC-K5QRWCG-34HH7V5-W7NXUAK"; };
+        "mac" = { id = "LYKOTLD-IKWRMXL-TLD3Q53-QSTDT2Z-PIGLR7I-JUPFVDW-LQ6P55J-TPKUOA5"; };
         "phone" = { id = "Y7X4JVO-CBFLCEV-DI6RXIL-Z3H7H5U-D5EPPZR-NO2JITE-WE4KYLJ-BZHCIQQ"; };
       };
 
       folders = {
         "KeePass" = {
           path = "/home/${userConfig.username}/Sync/KeePass";
-          devices = [ "desktop" "phone" ];
+          devices = [ "desktop" "mac" "phone" ];
         };
       };
     };
