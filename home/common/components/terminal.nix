c213273c31f0cbbd8e57c7cd7db6a2eb784fd0ec
ultomnis <125839032+ultomnis@users.noreply.github.com>
2024-10-19 13:17:15 -0400

    cmus: Configure theme

--- home/common/components/terminal.nix
+++ home/common/components/terminal.nix
@@ -4,7 +4,6 @@
   # CLI/TUI programs
   home.packages = with pkgs; [
     bc
-    cmus
     exiftool
     ffmpeg
     jmtpfs
--- home/common/programs/default.nix
+++ home/common/programs/default.nix
@@ -1,6 +1,7 @@
 {
   imports = [
     ./alacritty.nix
+    ./cmus.nix
     ./firefox.nix
     ./fish.nix
     ./git.nix
