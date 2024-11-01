1a0662eb7802450bc5f0ccb05ba496e74e4a583c
ultomnis <125839032+ultomnis@users.noreply.github.com>
2024-11-01 13:33:20 -0400

    Move mullvad-vpn to services

--- modules/nixos/programs/default.nix
+++ modules/nixos/programs/default.nix
@@ -2,7 +2,6 @@
   imports = [
     ./adb.nix
     ./fish.nix
-    ./mullvad-vpn.nix
     ./steam.nix
     ./stylix.nix
     ./sway.nix
--- modules/nixos/services/default.nix
+++ modules/nixos/services/default.nix
@@ -2,6 +2,7 @@
   imports = [
     ./avahi.nix
     ./greetd.nix
+    ./mullvad-vpn.nix
     ./ratbagd.nix
     ./syncthing.nix
   ];
