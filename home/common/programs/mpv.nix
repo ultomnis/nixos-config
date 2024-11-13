932533851b7d59b94001125ad28f6c9fd978ae7c
ultomnis <125839032+ultomnis@users.noreply.github.com>
2024-11-12 19:40:25 -0500

    mpv: Install uosc and set volume

--- home/common/programs/mpv.nix
+++ home/common/programs/mpv.nix
@@ -1,15 +1,21 @@
+{ pkgs, ... }:
+
 {
   # Media player
   programs.mpv = {
     enable = true;
 
-    # mpv configuration
     config = {
       deband = "no";
       keep-open = "yes";
       osd-bar = "no";
       profile = "high-quality";
       vo = "gpu-next";
+      volume = 50;
     };
+
+    scripts = with pkgs.mpvScripts; [
+      uosc
+    ];
   };
 }
