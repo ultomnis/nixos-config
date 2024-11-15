91cecd44481989aa457cbe53ab5c02391d188c37
ultomnis <125839032+ultomnis@users.noreply.github.com>
2024-11-15 10:08:12 -0500

    networking: Adapt rpfilter

--- modules/nixos/system/networking.nix
+++ modules/nixos/system/networking.nix
@@ -7,9 +7,19 @@
 
     firewall = rec {
       allowedTCPPorts = [
-        53317
+        53317 # LocalSend
       ];
       allowedUDPPorts = allowedTCPPorts;
+
+      # WireGuard traffic
+      extraCommands = ''
+        ip46tables -t mangle -I nixos-fw-rpfilter -p udp -m udp --sport 51820 -j RETURN
+        ip46tables -t mangle -I nixos-fw-rpfilter -p udp -m udp --dport 51820 -j RETURN
+      '';
+      extraStopCommands = ''
+        ip46tables -t mangle -D nixos-fw-rpfilter -p udp -m udp --sport 51820 -j RETURN || true
+        ip46tables -t mangle -D nixos-fw-rpfilter -p udp -m udp --dport 51820 -j RETURN || true
+      '';
     };
   };
   users.users.${userConfig.username}.extraGroups = [ "networkmanager" ];
