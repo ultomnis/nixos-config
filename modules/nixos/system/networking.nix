d05d23333d5f6bbbca976ef4b87c05426aad24e1
ultomnis <125839032+ultomnis@users.noreply.github.com>
2024-11-16 15:06:59 -0500

    networking: Remove extra commands

--- modules/nixos/system/networking.nix
+++ modules/nixos/system/networking.nix
@@ -12,16 +12,6 @@
       allowedUDPPorts = [
         53317 # LocalSend
       ];
-
-      # WireGuard traffic
-      extraCommands = ''
-        ip46tables -t mangle -I nixos-fw-rpfilter -p udp -m udp --sport 51820 -j RETURN
-        ip46tables -t mangle -I nixos-fw-rpfilter -p udp -m udp --dport 51820 -j RETURN
-      '';
-      extraStopCommands = ''
-        ip46tables -t mangle -D nixos-fw-rpfilter -p udp -m udp --sport 51820 -j RETURN || true
-        ip46tables -t mangle -D nixos-fw-rpfilter -p udp -m udp --dport 51820 -j RETURN || true
-      '';
     };
   };
 
