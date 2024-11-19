d18879b6d5c0dc570d5cf2713cd4e274ecd64b4e
ultomnis <125839032+ultomnis@users.noreply.github.com>
2024-11-19 17:42:01 -0500

    jankyborders: Change colors

--- modules/darwin/programs/jankyborders.nix
+++ modules/darwin/programs/jankyborders.nix
@@ -1,11 +1,9 @@
-{ config, ... }:
-
 {
   services.jankyborders = {
     enable = true;
 
-    active_color = "0xff${config.lib.stylix.colors.base0D}";
-    inactive_color = "0xff${config.lib.stylix.colors.base03}";
+    active_color = "0xccf0f0f0";
+    inactive_color = "0x00ffffff";
     
     hidpi = true;
     width = 5.0;
