8b6e79b250606254ad82d35083a9baeb0d755ee4
ultomnis <125839032+ultomnis@users.noreply.github.com>
2024-11-26 18:18:25 -0500

    Enable nm-applet

--- modules/nixos/daemons/networking.nix
+++ modules/nixos/daemons/networking.nix
@@ -4,4 +4,7 @@
   # Configure network interfaces
   networking.networkmanager.enable = true;
   users.users.${userConfig.username}.extraGroups = [ "networkmanager" ];
+
+  # System tray applet
+  programs.nm-applet.enable = true;
 }
