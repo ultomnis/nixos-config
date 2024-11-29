da5c54a14eabbc7009be07fe757b2fa24bb54834
ultomnis <125839032+ultomnis@users.noreply.github.com>
2024-11-29 12:20:16 -0500

    Fix issue with opening links

--- home/nixos/desktop/sway.nix
+++ home/nixos/desktop/sway.nix
@@ -16,6 +16,9 @@
         {
           command = "${pkgs.polkit_gnome}/libexec/polkit-gnome-authentication-agent-1";
         }
+        {
+          command = "${pkgs.systemd}/bin/systemctl --user import-environment PATH";
+        }
       ];
 
       input = {
