7be8e1d0b10670473435cb875949b6ed9cd0ac50
ultomnis <125839032+ultomnis@users.noreply.github.com>
2024-12-01 14:35:38 -0500

    sway: Remove import-environment command

--- home/nixos/desktop/sway.nix
+++ home/nixos/desktop/sway.nix
@@ -21,9 +21,6 @@
         {
           command = "${pkgs.polkit_gnome}/libexec/polkit-gnome-authentication-agent-1";
         }
-        {
-          command = "${pkgs.systemd}/bin/systemctl --user import-environment PATH";
-        }
       ];
 
       input = {
