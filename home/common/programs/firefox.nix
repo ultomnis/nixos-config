c241e7ac6d57ee33d07d1306cfc40a9fd079edf1
ultomnis <125839032+ultomnis@users.noreply.github.com>
2024-12-02 14:24:44 -0500

    firefox: Switch to firefox-beta and modify settings

--- home/common/programs/firefox.nix
+++ home/common/programs/firefox.nix
@@ -1,16 +1,13 @@
-{ lib, pkgs, ... }:
+{ pkgs, ... }:
 
 {
   programs.firefox = {
     enable = true;
 
-    package = lib.mkIf pkgs.stdenv.isDarwin pkgs.firefox-unwrapped;
+    package = if pkgs.stdenv.isDarwin then pkgs.firefox-beta-unwrapped else pkgs.firefox-beta;
 
-    # Attribute set of profiles
     profiles = {
       default = {
-        id = 0; # Set default profile
-
         # Firefox add-ons
         extensions = with pkgs.nur.repos.rycee.firefox-addons; [
           darkreader
@@ -28,10 +25,6 @@
 
         # Firefox preferences
         settings = {
-          "app.update.auto" = false; # AppAutoUpdate
-          "browser.crashReports.unsubmittedCheck.autoSubmit2" = false; # Allow Firefox to send backlogged crash reports
-          "browser.formfill.enable" = false; # DisableFormHistory
-
           # DisableFirefoxStudies
           "browser.newtabpage.activity-stream.asrouter.userprefs.cfr.addons" = false;
           "browser.newtabpage.activity-stream.asrouter.userprefs.cfr.features" = false;
@@ -44,36 +37,18 @@
           "browser.newtabpage.activity-stream.showSponsored" = false;
           "browser.newtabpage.activity-stream.showSponsoredTopSites" = false;
 
-          # SearchSuggestEnabled
-          "browser.search.suggest.enabled" = false;
-          "browser.urlbar.suggest.searches" = false;
-
-          "browser.toolbars.bookmarks.visibility" = "never"; # DisplayBookmarksToolbar
-
-          # FirefoxSuggest
-          "browser.urlbar.quicksuggest.dataCollection.enabled" = false;
-          "browser.urlbar.suggest.quicksuggest.nonsponsored" = false;
-          "browser.urlbar.suggest.quicksuggest.sponsored" = false;
-
           # DisableTelemetry
           "datareporting.healthreport.uploadEnabled" = false;
           "datareporting.policy.dataSubmissionEnabled" = false;
           "toolkit.telemetry.archive.enabled" = false;
 
-          "dom.private-attribution.submission.enabled" = false; # Allow websites to perform privacy-preserving ad measurement
           "dom.security.https_only_mode" = true; # HttpsOnlyMode
-          "extensions.pocket.enabled" = false; # DisablePocket
-          "extensions.screenshots.disabled" = true; # DisableFirefoxScreenshots
-          "extensions.update.enabled" = false; # ExtensionUpdate
-          "identity.fxaccounts.enabled" = false; # DisableFirefoxAccounts
 
           # EnableTrackingProtection
           "privacy.trackingprotection.enabled" = true;
           "privacy.trackingprotection.cryptomining.enabled" = true;
           "privacy.trackingprotection.fingerprinting.enabled" = true;
           "privacy.trackingprotection.pbmode.enabled" = true;
-
-          "signon.rememberSignons" = false; # OfferToSaveLogins
         };
       };
     };
