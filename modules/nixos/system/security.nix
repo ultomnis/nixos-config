e277dd81accf0fa6005933b291071e8b67e97d34
ultomnis <125839032+ultomnis@users.noreply.github.com>
2024-12-10 10:41:34 -0500

    security: Enable realtime

--- modules/nixos/system/security.nix
+++ modules/nixos/system/security.nix
@@ -1,5 +1,21 @@
 {
   security = {
+    # Pluggable Authentication Modules
+    pam = {
+      # Realtime priority
+      loginLimits = [
+        {
+          domain = "@users";
+          item = "rtprio";
+          type = "-";
+          value = 1;
+        }
+      ];
+    };
+
+    # RealtimeKit system service
+    rtkit.enable = true;
+
     sudo.extraConfig = ''
       Defaults passprompt="What? Make it yourself:"
     '';
