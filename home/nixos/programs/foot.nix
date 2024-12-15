e7c8c4325cda0d6ba26fc95d144169e3a1900c9d
ultomnis <125839032+ultomnis@users.noreply.github.com>
2024-12-15 17:41:02 -0500

    foot: Add padding

--- home/nixos/programs/foot.nix
+++ home/nixos/programs/foot.nix
@@ -10,6 +10,7 @@
       main = {
         font = "Hack Nerd Font Mono:size=11";
         include = "${pkgs.foot.themes}/share/foot/themes/rose-pine";
+        pad = "5x5";
       };
 
       bell = {
