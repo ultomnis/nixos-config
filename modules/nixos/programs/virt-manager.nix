e6b2fdfcf9d22dcc9ab10783f1bea15ace78e3b8
ultomnis <125839032+ultomnis@users.noreply.github.com>
2024-12-14 20:03:42 -0500

    virt-manager: Update comment

--- modules/nixos/programs/virt-manager.nix
+++ modules/nixos/programs/virt-manager.nix
@@ -1,7 +1,7 @@
 { userConfig, ... }:
 
 {
-  # Virt-manager
+  # Manage virtual machines via libvirt
   programs.virt-manager.enable = true;
   virtualisation.libvirtd.enable = true;
   users.users.${userConfig.username}.extraGroups = [ "libvirtd" ];
