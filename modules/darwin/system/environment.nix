8a02cd0b3dfab730c5cd7f04503e6fd711e53a8f
ultomnis <125839032+ultomnis@users.noreply.github.com>
2025-03-13 14:18:06 -0400

    darwin: Set bitwarden as ssh-agent

--- modules/darwin/system/environment.nix
+++ modules/darwin/system/environment.nix
@@ -1,7 +1,10 @@
+{ userConfig, ... }:
+
 {
   environment = {
     variables = {
       HOMEBREW_NO_ANALYTICS = "1";
+      SSH_AUTH_SOCK = "/Users/${userConfig.name}/.bitwarden-ssh-agent.sock";
     };
   };
 }
