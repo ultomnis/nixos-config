ce94d5d714d99963b066336ceb3d77d08a800352
ultomnis <125839032+ultomnis@users.noreply.github.com>
2025-03-25 18:09:49 -0400

    ssh: Remove agent and keychain options

--- home/common/programs/ssh.nix
+++ home/common/programs/ssh.nix
@@ -4,20 +4,14 @@
   # Secure Shell
   programs.ssh = {
     enable = true;
-    addKeysToAgent = "yes";
 
     matchBlocks = {
-      "github.com" = {
+      "github" = {
+        user = "git";
         hostname = "github.com";
         identitiesOnly = true;
         identityFile = "${config.home.homeDirectory}/.ssh/github";
-        user = "git";
       };
     };
-
-    extraConfig = ''
-      IgnoreUnknown UseKeychain
-      UseKeychain yes
-    '';
   };
 }
