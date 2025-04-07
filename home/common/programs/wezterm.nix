a4e76a362a232d619bbe226d8a2747b14f546e16
ultomnis <125839032+ultomnis@users.noreply.github.com>
2025-04-07 17:14:12 -0400

    Set shell using wezterm

--- home/common/programs/wezterm.nix
+++ home/common/programs/wezterm.nix
@@ -1,4 +1,9 @@
-{ vars, ... }:
+{
+  lib,
+  pkgs,
+  vars,
+  ...
+}:
 
 {
   programs.wezterm = {
@@ -8,6 +13,7 @@
       local config = {}
 
       config.color_scheme = 'rose-pine'
+      config.default_prog = { '${lib.getExe pkgs.fish}' }
       config.enable_tab_bar = false
       config.font = wezterm.font 'Hack Nerd Font Mono'
       config.font_size = ${toString vars.fontSize}
--- modules/darwin/system/users.nix
+++ modules/darwin/system/users.nix
@@ -3,12 +3,9 @@
 {
   # User configuration
   users = {
-    knownUsers = [ "${vars.userConfig.name}" ];
-
     users.${vars.userConfig.name} = {
-      description = "user";
       home = "/Users/${vars.userConfig.name}";
-      uid = 501;
+      description = "user";
     };
   };
 }
