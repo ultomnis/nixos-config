5e7521e89fb22f0cb6eb0b4f42fb5b184846bd8d
ultomnis <125839032+ultomnis@users.noreply.github.com>
2025-04-10 15:49:13 -0400

    bluez: Disable hw volume

--- modules/nixos/daemons/pipewire.nix
+++ modules/nixos/daemons/pipewire.nix
@@ -6,5 +6,11 @@
     alsa.support32Bit = true;
     pulse.enable = true;
     jack.enable = true; # JACK applications
+
+    wireplumber.extraConfig."80-bluez-properties" = {
+      "monitor.bluez.properties" = {
+        "bluez5.enable-hw-volume" = false;
+      };
+    };
   };
 }
