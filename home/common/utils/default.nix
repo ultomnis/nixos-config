f7bf6f21b4b1fbca90e96fa56b86f7a03473a8ed
ultomnis <125839032+ultomnis@users.noreply.github.com>
2025-04-10 13:52:21 -0400

    Add eza

--- home/common/utils/default.nix
+++ home/common/utils/default.nix
@@ -1,6 +1,7 @@
 {
   imports = [
     ./btop.nix
+    ./eza.nix
     ./zoxide.nix
   ];
 }
---
+++ home/common/utils/eza.nix
@@ -0,0 +1,8 @@
+{
+  # Alternative to ls
+  programs.eza = {
+    enable = true;
+    git = true;
+    icons = "auto";
+  };
+}
