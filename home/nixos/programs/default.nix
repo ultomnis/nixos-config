8293120fbdee67b154cbd4fd49147b8f96bcb054
ultomnis <125839032+ultomnis@users.noreply.github.com>
2025-04-11 14:49:56 -0400

    Add imv

--- home/nixos/programs/default.nix
+++ home/nixos/programs/default.nix
@@ -1,6 +1,7 @@
 {
   imports = [
     ./gnome-keyring.nix
+    ./imv.nix
     ./mangohud.nix
     ./polkit-gnome.nix
     ./wlsunset.nix
---
+++ home/nixos/programs/imv.nix
@@ -0,0 +1,12 @@
+{
+  # Image viewer
+  programs.imv = {
+    enable = true;
+
+    settings = {
+      options = {
+        overlay_position_bottom = true;
+      };
+    };
+  };
+}
