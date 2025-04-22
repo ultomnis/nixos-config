eadce3de3d6143ee69a7819d4e680f575596a961
ultomnis <125839032+ultomnis@users.noreply.github.com>
2025-04-22 16:28:33 -0400

    Add terminal and homebrew environment variables

--- modules/common/system/variables.nix
+++ modules/common/system/variables.nix
@@ -8,6 +8,7 @@
 let
   inherit (config.luminosity.system)
     configurations
+    programs
     selections
     ;
 
@@ -17,9 +18,13 @@ in
     environment = {
       variables = {
         EDITOR = lib.mkIf (selections.editor != null) selections.editor;
+        HOMEBREW_NO_ANALYTICS = lib.mkIf (programs.homebrew.enable) "1";
+
         SSH_AUTH_SOCK = lib.mkIf (configurations.variables.bitwarden.sshAgent.enable) "${
           config.users.users.${userName}.home
         }/.bitwarden-ssh-agent.sock";
+
+        TERMINAL = lib.mkIf (selections.terminal != null) selections.terminal;
       };
     };
   };
