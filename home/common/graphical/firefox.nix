71c65e0db2b46a6b704969e50dce4a28150eccca
ultomnis <125839032+ultomnis@users.noreply.github.com>
2025-05-02 19:11:10 -0400

    firefox: Remove override

--- home/common/graphical/firefox.nix
+++ home/common/graphical/firefox.nix
@@ -1,9 +1,4 @@
-{
-  config,
-  lib,
-  pkgs,
-  ...
-}:
+{ config, lib, ... }:
 
 let
   cfg = config.luminosity.home.programs.graphical.firefox;
@@ -15,65 +10,63 @@ in
     programs.firefox = {
       enable = true;
 
-      package = pkgs.firefox.override {
-        # Set Firefox policies
-        extraPolicies = {
-          DisableFirefoxStudies = true;
-          DisableFormHistory = true;
-          DisablePocket = true;
-          DisableTelemetry = true;
-          DisplayBookmarksToolbar = "never";
-          NoDefaultBookmarks = true;
-          OfferToSaveLogins = false;
-          SearchSuggestEnabled = false;
+      # Set Firefox policies
+      policies = {
+        DisableFirefoxStudies = true;
+        DisableFormHistory = true;
+        DisablePocket = true;
+        DisableTelemetry = true;
+        DisplayBookmarksToolbar = "never";
+        NoDefaultBookmarks = true;
+        OfferToSaveLogins = false;
+        SearchSuggestEnabled = false;
 
-          EnableTrackingProtection = {
-            Value = true;
-            Locked = true;
-            Cryptomining = true;
-            Fingerprinting = true;
-          };
+        EnableTrackingProtection = {
+          Value = true;
+          Locked = true;
+          Cryptomining = true;
+          Fingerprinting = true;
+        };
 
-          ExtensionSettings =
-            let
-              extensionSetting =
-                extensionName: extraOptions:
-                {
-                  installation_mode = "force_installed";
-                  install_url = "https://addons.mozilla.org/firefox/downloads/latest/${extensionName}/latest.xpi";
-                }
-                // extraOptions;
-            in
-            {
-              "*" = {
-                installation_mode = "blocked";
-              };
-
-              "{446900e4-71c2-419f-a6a7-df9c091e268b}" = extensionSetting "bitwarden-password-manager" { };
-
-              "addon@darkreader.org" = extensionSetting "darkreader" { };
-
-              "uBlock0@raymondhill.net" = extensionSetting "ublock-origin" {
-                private_browsing = true;
-              };
+        ExtensionSettings =
+          let
+            extensionSetting =
+              extensionName: extraOptions:
+              {
+                installation_mode = "force_installed";
+                install_url = "https://addons.mozilla.org/firefox/downloads/latest/${extensionName}/latest.xpi";
+              }
+              // extraOptions;
+          in
+          {
+            "*" = {
+              installation_mode = "blocked";
             };
 
-          FirefoxHome = {
-            TopSites = false;
-            SponsoredTopSites = false;
-            Highlights = false;
-            Pocket = false;
-            SponsoredPocket = false;
-            Snippets = false;
-            Locked = true;
+            "{446900e4-71c2-419f-a6a7-df9c091e268b}" = extensionSetting "bitwarden-password-manager" { };
+
+            "addon@darkreader.org" = extensionSetting "darkreader" { };
+
+            "uBlock0@raymondhill.net" = extensionSetting "ublock-origin" {
+              private_browsing = true;
+            };
           };
 
-          FirefoxSuggest = {
-            WebSuggestions = false;
-            SponsoredSuggestions = false;
-            ImproveSuggest = false;
-            Locked = true;
-          };
+        FirefoxHome = {
+          TopSites = false;
+          SponsoredTopSites = false;
+          Highlights = false;
+          Pocket = false;
+          SponsoredPocket = false;
+          Snippets = false;
+          Locked = true;
+        };
+
+        FirefoxSuggest = {
+          WebSuggestions = false;
+          SponsoredSuggestions = false;
+          ImproveSuggest = false;
+          Locked = true;
         };
       };
 
