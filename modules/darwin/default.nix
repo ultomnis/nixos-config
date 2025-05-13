64392abd551b3aede30d11b608b0d7a7b79afcfd
ultomnis <125839032+ultomnis@users.noreply.github.com>
2025-05-13 15:19:36 -0400

    Reorganize options & folders in modules

--- modules/darwin/default.nix
+++ modules/darwin/default.nix
@@ -1,6 +1,6 @@
 {
   imports = [
+    ./configurations
     ./programs
-    ./system
   ];
 }
--- modules/nixos/default.nix
+++ modules/nixos/default.nix
@@ -1,7 +1,7 @@
 {
   imports = [
+    ./configurations
     ./programs
     ./services
-    ./system
   ];
 }
