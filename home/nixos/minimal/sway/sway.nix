fb23e36928a768bb02239f435721034cfe2bd3b1
ultomnis <125839032+ultomnis@users.noreply.github.com>
2025-06-14 16:28:18 -0400

    sway: Remove mkOptionDefault

--- home/nixos/minimal/sway/sway.nix
+++ home/nixos/minimal/sway/sway.nix
@@ -77,12 +77,70 @@ in
 
         keybindings =
           let
-            modifier = config.wayland.windowManager.sway.config.modifier;
-          in
-          lib.mkOptionDefault {
-            "--no-repeat ${modifier}+Print" = ''exec ${lib.getExe pkgs.grim} -g "$(${lib.getExe pkgs.slurp})"'';
+            menu = config.wayland.windowManager.sway.config.menu;
+            mod = config.wayland.windowManager.sway.config.modifier;
+            term = config.wayland.windowManager.sway.config.terminal;
 
-            "${modifier}+Escape" = "exec ${lib.getExe pkgs.swaylock} -f";
+          in
+          {
+            "${mod}+Return" = "exec ${term}";
+            "${mod}+Shift+q" = "kill";
+            "${mod}+d" = "exec ${menu}";
+            "${mod}+Shift+c" = "reload";
+
+            "${mod}+h" = "focus left";
+            "${mod}+j" = "focus down";
+            "${mod}+k" = "focus up";
+            "${mod}+l" = "focus right";
+
+            "${mod}+Shift+h" = "move left";
+            "${mod}+Shift+j" = "move down";
+            "${mod}+Shift+k" = "move up";
+            "${mod}+Shift+l" = "move right";
+
+            "${mod}+1" = "workspace number 1";
+            "${mod}+2" = "workspace number 2";
+            "${mod}+3" = "workspace number 3";
+            "${mod}+4" = "workspace number 4";
+            "${mod}+5" = "workspace number 5";
+            "${mod}+6" = "workspace number 6";
+            "${mod}+7" = "workspace number 7";
+            "${mod}+8" = "workspace number 8";
+            "${mod}+9" = "workspace number 9";
+            "${mod}+0" = "workspace number 10";
+
+            "${mod}+Shift+1" = "move container to workspace number 1";
+            "${mod}+Shift+2" = "move container to workspace number 2";
+            "${mod}+Shift+3" = "move container to workspace number 3";
+            "${mod}+Shift+4" = "move container to workspace number 4";
+            "${mod}+Shift+5" = "move container to workspace number 5";
+            "${mod}+Shift+6" = "move container to workspace number 6";
+            "${mod}+Shift+7" = "move container to workspace number 7";
+            "${mod}+Shift+8" = "move container to workspace number 8";
+            "${mod}+Shift+9" = "move container to workspace number 9";
+            "${mod}+Shift+0" = "move container to workspace number 10";
+
+            "${mod}+b" = "splith";
+            "${mod}+v" = "splitv";
+
+            "${mod}+s" = "layout stacking";
+            "${mod}+w" = "layout tabbed";
+            "${mod}+e" = "layout toggle split";
+
+            "${mod}+f" = "fullscreen";
+
+            "${mod}+Shift+space" = "floating toggle";
+
+            "${mod}+space" = "focus mode_toggle";
+
+            "${mod}+a" = "focus parent";
+
+            "${mod}+Shift+minus" = "move scratchpad";
+            "${mod}+minus" = "scratchpad show";
+
+            "${mod}+r" = "mode resize";
+
+            "${mod}+Escape" = "exec ${lib.getExe pkgs.swaylock} -f";
 
             "--locked XF86AudioMute" =
               "exec ${lib.getExe' pkgs.wireplumber "wpctl"} set-mute @DEFAULT_AUDIO_SINK@ toggle";
@@ -96,8 +154,26 @@ in
             "--locked XF86AudioPrev" = "exec ${lib.getExe pkgs.playerctl} previous";
             "--locked XF86AudioNext" = "exec ${lib.getExe pkgs.playerctl} next";
             "--locked XF86AudioPlay" = "exec ${lib.getExe pkgs.playerctl} play-pause";
+            "--locked XF86AudioPause" = "exec ${lib.getExe pkgs.playerctl} play-pause";
             "--locked XF86AudioStop" = "exec ${lib.getExe pkgs.playerctl} stop";
+
+            "--locked XF86MonBrightnessDown" = "exec ${lib.getExe pkgs.brightnessctl} set 5%-";
+            "--locked XF86MonBrightnessUp" = "exec ${lib.getExe pkgs.brightnessctl} set 5%+";
+
+            "--no-repeat ${mod}+Print" = ''exec ${lib.getExe pkgs.grim} -g "$(${lib.getExe pkgs.slurp})"'';
           };
+
+        modes = {
+          resize = {
+            "h" = "resize shrink width 10px";
+            "j" = "resize grow height 10px";
+            "k" = "resize shrink height 10px";
+            "l" = "resize grow width 10px";
+
+            "Return" = "mode default";
+            "Escape" = "mode default";
+          };
+        };
 
         bars = lib.singleton {
           command = lib.getExe pkgs.waybar;
