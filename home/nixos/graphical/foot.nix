1e249ef5a243fd3f8ed948dc8017fde9310fe6ad
ultomnis <125839032+ultomnis@users.noreply.github.com>
2025-06-24 16:23:35 -0400

    foot: Fix colors

--- home/nixos/graphical/foot.nix
+++ home/nixos/graphical/foot.nix
@@ -15,18 +15,23 @@ in
     inherit (cfg) enable;
 
     settings = {
-      colors = {
-        alpha = 0.6;
-        foreground = "ffffff";
-        background = "000000";
-      };
+      colors.alpha = 0.6;
 
       main = {
         font = lib.mkIf (
           cfg.font.name != null && cfg.font.size != null
         ) "${cfg.font.name}:size=${toString cfg.font.size}";
 
-        include = lib.optional (cfg.theme != null) "${pkgs.foot.themes}/share/foot/themes/${cfg.theme}";
+        include =
+          let
+            foot-colors = pkgs.writeText "foot-colors.ini" ''
+              [colors]
+              background=000000
+              foreground=ffffff
+            '';
+          in
+          lib.optional (cfg.theme != null) "${pkgs.foot.themes}/share/foot/themes/${cfg.theme}"
+          ++ lib.singleton (toString foot-colors);
 
         pad = "5x5";
         shell = lib.mkIf (cfg.shell != null) (lib.getExe pkgs.${cfg.shell});
