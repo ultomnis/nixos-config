8de90e6062b1303855b63e651617bf5ad544ba7a
ultomnis <125839032+ultomnis@users.noreply.github.com>
2025-06-26 17:09:10 -0400

    mpv: Use inherit

--- home/common/graphical/mpv.nix
+++ home/common/graphical/mpv.nix
@@ -1,37 +1,35 @@
-{ config, lib, ... }:
+{ config, ... }:
 
 let
   cfg = config.luminosity.home.programs.graphical.mpv;
 
 in
 {
-  config = lib.mkIf cfg.enable {
-    # Media player
-    programs.mpv = {
-      enable = true;
+  # Media player
+  programs.mpv = {
+    inherit (cfg) enable;
 
-      config = {
-        # Program behavior
-        profile = "high-quality";
+    config = {
+      # Program behavior
+      profile = "high-quality";
 
-        # Video
-        vo = "gpu-next";
+      # Video
+      vo = "gpu-next";
 
-        # Audio
-        volume = 80;
+      # Audio
+      volume = 80;
 
-        # Window
-        keep-open = "yes";
+      # Window
+      keep-open = "yes";
 
-        # OSD
-        osd-bar = "no";
+      # OSD
+      osd-bar = "no";
 
-        # GPU renderer options
-        interpolation = "yes";
+      # GPU renderer options
+      interpolation = "yes";
 
-        # Miscellaneous
-        video-sync = "display-resample";
-      };
+      # Miscellaneous
+      video-sync = "display-resample";
     };
   };
 }
