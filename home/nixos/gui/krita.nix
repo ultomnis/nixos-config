1b9992c066a3ca246f154cc438d61d83dfd16587
ultomnis <125839032+ultomnis@users.noreply.github.com>
2025-09-18 21:13:51 -0400

    krita: Add qtimageformats

--- home/nixos/gui/krita.nix
+++ home/nixos/gui/krita.nix
@@ -12,7 +12,11 @@ in
 {
   config = lib.mkIf cfg.enable {
     home.packages = [
-      pkgs.krita
+      (pkgs.krita.overrideAttrs (oldAttrs: {
+        buildInputs = oldAttrs.buildInputs ++ [
+          pkgs.libsForQt5.qt5.qtimageformats
+        ];
+      }))
     ];
   };
 }
